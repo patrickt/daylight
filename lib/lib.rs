6fef1d726781a179581971d76288432b7a182d6d
Patrick Thomson <patrick.thomson@bubble.io>
2025-12-09 16:04:39 -0500

    Remove vestigial #[path] specifiers.

--- lib/lib.rs
+++ lib/lib.rs
@@ -1,15 +1,10 @@
+pub mod client;
+pub mod languages;
+pub mod server;
+
 #[path = "generated/daylight_generated.rs"]
 #[allow(warnings)]
 pub mod daylight_generated;
-
-#[path = "client.rs"]
-pub mod client;
-
-#[path = "languages.rs"]
-pub mod languages;
-
-#[path = "server.rs"]
-pub mod server;
 
 #[cfg(test)]
 #[path = "server_tests.rs"]
